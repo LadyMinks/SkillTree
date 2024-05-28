708e6d41f47f6c4d6c1d831927ed0011c00a72ae
LadyMinks <minka-96@outlook.com>
2024-05-28 15:45:42 +0200

    idunno what all that crap is tbh.

--- SkillTree/SkillTree.Server/obj/Release/net8.0/SkillTree.Server.AssemblyInfo.cs
+++ SkillTree/SkillTree.Server/obj/Release/net8.0/SkillTree.Server.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkillTree.Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ca03778c670050475b1d00f372d7a560786b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkillTree.Server")]
